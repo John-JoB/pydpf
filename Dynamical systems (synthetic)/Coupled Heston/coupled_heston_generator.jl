d5644936e58b2a26f672058b9a994b05e61cd486
Benjamin Cox <Benjamin.Cox@ed.ac.uk>
2024-10-07 18:05:21 +0100

    updated generation process

--- Dynamical systems (synthetic)/Coupled Heston/coupled_heston_generator.jl
+++ Dynamical systems (synthetic)/Coupled Heston/coupled_heston_generator.jl
@@ -8,17 +8,25 @@ using DifferentialEquations.EnsembleAnalysis
 import RandomNumbers: Xorshifts
 
 
-Random.seed!(7734)
+Random.seed!(1)
 
-μ = log(1 + 0.10) / 252
+μ = log(1 + 0.07) / 252
 # θ = 6.47e-5
 # κ = 0.097
 # σ = 5.38e-3
 # ρ = -0.5
-θ = 0.0055 / sqrt(252)
-κ = 5.0
-σ = 0.3 / sqrt(252)
+
+# θ = 0.0055 / sqrt(252)
+θ = 0.005 / sqrt(252)
+
+# κ = 5.0
+κ = 0.05
+
+σ = 0.2 / sqrt(252)
+
 ρ = -0.30
+# ρ = 0.0
+
 v0 = θ
 tspan = (0.0, 252.0)
 
@@ -63,16 +71,16 @@ heston_model = HestonProblem(μ, κ, θ, σ, ρ, [50.0, v0], tspan, seed=rand(UI
 
 sol = solve(heston_model, SRIW1(), dt=1e-2, adaptive=false, saveat=tspan[1]:0.1:tspan[2], isoutofdomain=check_domain)
 ensemble_prob = EnsembleProblem(heston_model, prob_func=reseed_heston, safetycopy=false)
-sim = solve(ensemble_prob, SRIW1(), dt=1e-2, adaptive=false, EnsembleThreads(), trajectories=2000, saveat=tspan[1]:1.0:tspan[2])
+sim = solve(ensemble_prob, SRIW1(), dt=1e-2, adaptive=false, EnsembleThreads(), trajectories=100, saveat=tspan[1]:1.0:tspan[2])
 
 print("Done simulating!")
 
-# p1 = plot(sol, lw=1.5, size=(666, 200), dpi=300, label="", idxs=(0, 1), ylabel="Price")
-# p2 = plot(sol, lw=1.5, size=(666, 200), dpi=300, label="", idxs=((t, v) -> (t, v), 0, 2), ylabel="Sq. Volatility")
-# plot(p1, p2, layout=(2, 1), size=(600, 600))
+p1 = plot(sol, lw=1.5, size=(666, 200), dpi=300, label="", idxs=(0, 1), ylabel="Price")
+p2 = plot(sol, lw=1.5, size=(666, 200), dpi=300, label="", idxs=((t, v) -> (t, v), 0, 2), ylabel="Sq. Volatility")
+plot(p1, p2, layout=(2, 1), size=(600, 600))
 
-summ = EnsembleSummary(sim)
-plot(summ)
+# summ = EnsembleSummary(sim)
+# plot(summ)
 # p1_ens = plot(sim, lw=1.5, size=(666, 200), dpi=300, label="", idxs=(0, 1), ylabel="Price")
 # p2_ens = plot(sim, lw=1.5, size=(666, 200), dpi=300, label="", idxs=((t, v) -> (t, v), 0, 2), ylabel="Sq. Volatility")
 # plot(p1_ens, p2_ens, layout=(2, 1), size=(600, 600))
--- Dynamical systems (synthetic)/Lorenz 96/lorenz96_generator.jl
+++ Dynamical systems (synthetic)/Lorenz 96/lorenz96_generator.jl
@@ -1,8 +1,8 @@
 using Pkg
 Pkg.activate("DynamicalSystemsGeneration")
 
-# using Plots;
-# plotlyjs();
+using Plots;
+plotlyjs();
 using Random
 using LinearAlgebra
 using DifferentialEquations
@@ -13,20 +13,28 @@ using Dates
 
 Random.seed!(123456789)
 
+# function lorenz!(du, u, p, t)
+#     N = length(du)
+#     F = p[1]
+#     du[1] = (u[2] - u[N-1]) * u[N] - u[1] + F
+#     du[2] = (u[3] - u[N]) * u[1] - u[2] + F
+#     du[N] = (u[1] - u[N-2]) * u[N-1] - u[N] + F
+#     for n in 3:(N-1)
+#         du[n] = (u[n+1] - u[n-2]) * u[n-1] - u[n] + F
+#     end
+#     nothing
+# end
+
 function lorenz!(du, u, p, t)
-    N = length(du)
-    F = p[1]
-    du[1] = (u[2] - u[N-1]) * u[N] - u[1] + F
-    du[2] = (u[3] - u[N]) * u[1] - u[2] + F
-    du[N] = (u[1] - u[N-2]) * u[N-1] - u[N] + F
-    for n in 3:(N-1)
-        du[n] = (u[n+1] - u[n-2]) * u[n-1] - u[n] + F
-    end
+    ρ, σ, β = (28.0, 10.0, 8.0 / 3.0)
+    du[1] = σ * (u[2] - u[1])
+    du[2] = u[1] * (ρ - u[3]) - u[2]
+    du[3] = u[1] * u[2] - β * u[3]
     nothing
 end
 
 function σ_lorenz(du, u, p, t)
-    du .= 0.05
+    du .= 5.0
 end
 
 function prob_func(prob, i, repeat)
@@ -39,50 +47,52 @@ end
 state_dimension = 25
 num_series = 1000
 delta_t = 0.01
-u0 = ones(state_dimension)
-u0[1] = 0.01
+# u0 = ones(state_dimension)
+# u0[1] = 0.01
+u0 = [1.0, 0.0, 0.0]
 
-tspan = (0.0, 10.0)
+tspan = (0.0, 15.0)
 
-prob = SDEProblem{true}(lorenz!, σ_lorenz, u0, tspan, [8,])
+prob = SDEProblem{true}(lorenz!, σ_lorenz, u0, tspan, [8,]);
 
-ensemble_prob = EnsembleProblem(prob, prob_func=prob_func, safetycopy=false)
-sim = solve(ensemble_prob, SOSRA(), EnsembleThreads(), trajectories=num_series, saveat=0.0:delta_t:20.0)
-# plot(sim, idxs=(1, 2, 3))
+# ensemble_prob = EnsembleProblem(prob, prob_func=prob_func, safetycopy=false)
+# sim = solve(ensemble_prob, SOSRA(), EnsembleThreads(), trajectories=num_series, saveat=0.0:delta_t:20.0)
+sim = solve(prob, SOSRA());
+plot(sim, idxs=(1, 2, 3))
 
-function compute_linear_observations(ensemble, H, R, delta_t)
-    U = (cholesky(R).U)'
-    obs_dim = size(H)[1]
-    num_sims = length(ensemble)
-    observation_array = Vector{Vector{Vector{Float64}}}(undef, num_sims)
-    Threads.@threads for i in eachindex(ensemble)
-        n_obs = length(ensemble[i].t)
-        observation_array[i] = (Ref(H) .* ensemble[i].u) .+ (Ref(U) .* randn.(fill(obs_dim, n_obs)))
-    end
-    return observation_array
-end
+# function compute_linear_observations(ensemble, H, R, delta_t)
+#     U = (cholesky(R).U)'
+#     obs_dim = size(H)[1]
+#     num_sims = length(ensemble)
+#     observation_array = Vector{Vector{Vector{Float64}}}(undef, num_sims)
+#     Threads.@threads for i in eachindex(ensemble)
+#         n_obs = length(ensemble[i].t)
+#         observation_array[i] = (Ref(H) .* ensemble[i].u) .+ (Ref(U) .* randn.(fill(obs_dim, n_obs)))
+#     end
+#     return observation_array
+# end
 
-sparsity_amount = 0.8
-H = rand(5, state_dimension)
-H .*= (rand(5, state_dimension) .> sparsity_amount)
-R = rand(5, 5)
-R *= R'
-R *= 1.0 ^ 2
+# sparsity_amount = 0.8
+# H = rand(5, state_dimension)
+# H .*= (rand(5, state_dimension) .> sparsity_amount)
+# R = rand(5, 5)
+# R *= R'
+# R *= 1.0 ^ 2
 
-observations = compute_linear_observations(sim, H, R, delta_t);
-print("Generated $num_series $state_dimension dimensional series of length $(length(sim[1].t)) and associated observations!")
+# observations = compute_linear_observations(sim, H, R, delta_t);
+# print("Generated $num_series $state_dimension dimensional series of length $(length(sim[1].t)) and associated observations!")
 
-# plot(sim[1].t, reduce(hcat, observations[1])')
+# # plot(sim[1].t, reduce(hcat, observations[1])')
 
-function save_sim_obs_to_file(path, sim, obs)
-    mkpath(path)
-    Threads.@threads for idx in eachindex(sim)
-        sim_dataframe = DataFrame(t = sim[idx].t, state = sim[idx].u, observation = obs[idx])
-        CSV.write(path * "$idx.csv", sim_dataframe)
-    end
-end
+# function save_sim_obs_to_file(path, sim, obs)
+#     mkpath(path)
+#     Threads.@threads for idx in eachindex(sim)
+#         sim_dataframe = DataFrame(t = sim[idx].t, state = sim[idx].u, observation = obs[idx])
+#         CSV.write(path * "$idx.csv", sim_dataframe)
+#     end
+# end
 
-current_datetime_fs_string = Dates.format(now(), "yyyy-mm-dd_HH-MM-SS")
-save_path = "./Lorenz 96/data/L96_SYSTEM_$(current_datetime_fs_string)_$(state_dimension)_DIM_STATE_$(size(H)[1])_DIM_OBS_WITH_$(sparsity_amount*100)PERCENT_SPARSITY/";
+# current_datetime_fs_string = Dates.format(now(), "yyyy-mm-dd_HH-MM-SS")
+# save_path = "./Lorenz 96/data/L96_SYSTEM_$(current_datetime_fs_string)_$(state_dimension)_DIM_STATE_$(size(H)[1])_DIM_OBS_WITH_$(sparsity_amount*100)PERCENT_SPARSITY/";
 
-# save_sim_obs_to_file(save_path, sim, observations)
+# # save_sim_obs_to_file(save_path, sim, observations)

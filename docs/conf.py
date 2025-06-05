417ad6c7c69e0465055820cb8e2caf9fb5a55289
John-JoB <jjbrady18x@gmail.com>
2025-06-05 15:19:10 +0100

    Readthedocs

--- docs/conf.py
+++
@@ -1,5 +0,0 @@
-project = 'pydpf'
-author = 'John-Joseph Brady'
-version = '1.0.0'
-release = '1.0.0'
-extensions = ['numpydoc']
---
+++ docs/source/conf.py
@@ -0,0 +1,9 @@
+import sys
+import os
+project = 'pydpf'
+author = 'John-Joseph Brady'
+version = '1.0.0'
+release = '1.0.0'
+extensions = ['numpydoc', 'autodoc']
+sys.path.insert(0, os.path.abspath('../../pydpf/'))
+master_doc = 'modules'

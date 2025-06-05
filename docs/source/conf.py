10398e2ac09ca525bd0dadcf4dfc09b65f177a9a
John-JoB <jjbrady18x@gmail.com>
2025-06-05 15:49:31 +0100

    Readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,6 @@ extensions = ['numpydoc', 'autodoc']
 master_doc = 'modules'
 sys.modules['numpy'] = MagicMock()
 sys.modules['torch'] = MagicMock()
-sys.modules['pydpf'] = MagicMock()
 sys.modules['pandas'] = MagicMock()
 sys.modules['polars'] = MagicMock()
 sys.path.insert(0, os.path.abspath('../../pydpf/'))

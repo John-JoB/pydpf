1e4522064ef4eef8de75be30171e7061063e0194
John-JoB <jjbrady18x@gmail.com>
2025-06-05 15:44:53 +0100

    Readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,8 +1,14 @@
 import sys
 import os
+from unittest.mock import MagicMock
 project = 'pydpf'
 author = 'John-Joseph Brady'
 version = '1.0.0'
 release = '1.0.0'
 extensions = ['numpydoc', 'autodoc']
 master_doc = 'modules'
+sys.modules['numpy'] = MagicMock()
+sys.modules['torch'] = MagicMock()
+sys.modules['pydpf'] = MagicMock()
+sys.modules['pandas'] = MagicMock()
+sys.modules['polars'] = MagicMock()

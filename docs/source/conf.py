7526108dcf5911891a38402631d5f7e36cb62f39
John-JoB <jjbrady18x@gmail.com>
2025-06-12 12:05:35 +0100

    Documentation test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,8 +4,9 @@ project = 'pydpf'
 author = 'John-Joseph Brady'
 version = '1.0.0'
 release = '1.0.0'
-extensions = ['numpydoc', 'autodoc']
+extensions = ['numpydoc', 'autodoc', 'sphinx.ext.mathjax', 'sphinx_rtd_theme']
 master_doc = 'modules'
 numpydoc_show_class_members = False
+html_theme = "pydata_sphinx_theme"
 sys.path.insert(0, os.path.abspath('../../pydpf/'))
 sys.path.append(os.path.abspath('../..'))

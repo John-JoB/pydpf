ce306851220fbd1c1d3a4fbe801df0e89c7375d9
John-JoB <jjbrady18x@gmail.com>
2026-03-11 14:01:23 +0000

    Improved documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,11 +1,67 @@
 import sys
 import os
+import inspect
+import pkgutil
+import importlib
+
+
+def get_package_members(package_name):
+    objects = {}
+
+    package = importlib.import_module(package_name)
+
+    for _, module_name, _ in pkgutil.walk_packages(package.__path__, package.__name__ + "."):
+        module = importlib.import_module(module_name)
+        objects[module_name] = []
+
+
+        for name, obj in inspect.getmembers(module):
+            if name.startswith("_"):
+                continue
+            if inspect.isclass(obj) and  obj.__module__ == module_name:
+                objects[module_name].append((name, "class"))
+            if inspect.isfunction(obj) and  obj.__module__ == module_name and obj.__qualname__.split(".")[0] == "MyClass":
+                objects[module_name].append((name, "function"))
+
+    return objects
+
+def generate_api_pages(app):
+    package_name = "pydpf"
+    objects = get_package_members(package_name)
+    api_dir = os.path.join(app.srcdir, "api")
+    os.makedirs(api_dir, exist_ok=True)
+
+    index_lines = ["API Reference", "=============\n", ".. toctree::", "   :maxdepth: 2\n"]
+
+    for module_name, members in objects.items():
+        module_file = os.path.join(api_dir, f"{module_name}.rst")
+        with open(module_file, "w") as f:
+            f.write(f"{module_name}\n{'=' * len(module_name)}\n\n")
+            f.write(".. autosummary::\n")
+            f.write("   :toctree: generated\n\n")
+            for name, typ in members:
+                f.write(f"   {module_name}.{name}\n")
+
+        # Add this module page to the API index
+        index_lines.append(f"   {module_name}")
+
+    # Write the API index page
+    with open(os.path.join(api_dir, "index.rst"), "w") as f:
+        f.write("\n".join(index_lines))
+
+
 project = 'pydpf'
 author = 'John-Joseph Brady'
 version = '1.0'
 release = '1.1.2'
-extensions = ['numpydoc', 'autodoc', 'sphinx.ext.mathjax', 'sphinx_rtd_theme', 'sphinx.ext.coverage']
-autodoc_mock_imports = ['joblib', 'numpy', 'polars', 'pandas']
+extensions = ['numpydoc', "sphinx.ext.autodoc", 'sphinx.ext.mathjax', 'sphinx_rtd_theme', 'sphinx.ext.coverage', "sphinx.ext.autosummary", "sphinx.ext.napoleon"]
+autosummary_generate = True
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": False,
+    "show-inheritance": True,
+    "inherited-members": False,
+}
 master_doc = 'index'
 numpydoc_show_class_members = False
 html_theme = "pydata_sphinx_theme"
@@ -42,3 +98,6 @@ html_theme_options = {
 }
 
 sys.path.insert(0, os.path.abspath('../../pydpf/'))
+
+def setup(app):
+    app.connect("builder-inited", generate_api_pages)
---
+++ docs/source/tutorial.rst
@@ -0,0 +1,3 @@
+Tutorial
+========
+To add shortly.
